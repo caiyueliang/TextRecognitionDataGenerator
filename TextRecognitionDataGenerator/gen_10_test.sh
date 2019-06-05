335ad2d9a4159b8547acfdeaa31e81ebf82dbd88
caiyueliang <393900414@qq.com>
2019-06-05 14:25:51 +0800

    update

---
+++ TextRecognitionDataGenerator/gen_10_test.sh
@@ -0,0 +1,20 @@
+#/bin/bash
+python run.py --language cn --length 10 --format 32 --loop_times 80 --output_dir "outs/train/black_1/"
+python run.py --language cn --length 10 --format 32 --loop_times 20 --output_dir "outs/test/black_1/"
+
+python run.py --language cn --length 10 --format 32 --loop_times 20 --output_dir "outs/train/black_1/" --text_color "#00B2EE"
+python run.py --language cn --length 10 --format 32 --loop_times 10 --output_dir "outs/test/black_1/" --text_color "#00B2EE"
+
+# ==========================================================================================================================================
+python run.py --language cn --length 10 --format 32 --loop_times 80 --output_dir "outs/train/black_3/" --space_width 3
+python run.py --language cn --length 10 --format 32 --loop_times 20 --output_dir "outs/test/black_3/" --space_width 3
+
+python run.py --language cn --length 10 --format 32 --loop_times 20 --output_dir "outs/train/black_3/" --text_color "#00B2EE" --space_width 3
+python run.py --language cn --length 10 --format 32 --loop_times 10 --output_dir "outs/test/black_3/" --text_color "#00B2EE" --space_width 3
+
+# ==========================================================================================================================================
+python run.py --language cn --length 10 --format 32 --loop_times 80 --output_dir "outs/train/black_5/" --space_width 5
+python run.py --language cn --length 10 --format 32 --loop_times 20 --output_dir "outs/test/black_5/" --space_width 5
+
+python run.py --language cn --length 10 --format 32 --loop_times 20 --output_dir "outs/train/black_5/" --text_color "#00B2EE" --space_width 5
+python run.py --language cn --length 10 --format 32 --loop_times 10 --output_dir "outs/test/black_5/" --text_color "#00B2EE" --space_width 5

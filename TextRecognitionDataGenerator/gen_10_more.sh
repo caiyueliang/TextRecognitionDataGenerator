b7e557701e9b4b376a5940de42e3f02a33898134
caiyueliang <393900414@qq.com>
2019-06-05 15:34:02 +0800

    update

--- TextRecognitionDataGenerator/gen_10_more.sh
+++ TextRecognitionDataGenerator/gen_10_more.sh
@@ -1,8 +1,8 @@
 #/bin/bash
-python run.py --language cn --length 12 --format 32 --loop_times 40 --output_dir "outs/train/black_1/"
-python run.py --language cn --length 12 --format 32 --loop_times 20 --output_dir "outs/test/black_1/"
-python run.py --language cn --length 12 --format 32 --loop_times 20 --output_dir "outs/train/black_1/" --text_color "#00B2EE"
-python run.py --language cn --length 12 --format 32 --loop_times 10 --output_dir "outs/test/black_1/" --text_color "#00B2EE"
+python run.py --language cn --length 14 --format 32 --loop_times 60 --output_dir "outs/train/black_1/"
+python run.py --language cn --length 14 --format 32 --loop_times 30 --output_dir "outs/test/black_1/"
+python run.py --language cn --length 14 --format 32 --loop_times 30 --output_dir "outs/train/black_1/" --text_color "#00B2EE"
+python run.py --language cn --length 14 --format 32 --loop_times 15 --output_dir "outs/test/black_1/" --text_color "#00B2EE"
 
 # ==========================================================================================================================================
 python run.py --language cn --length 10 --format 32 --loop_times 40 --output_dir "outs/train/black_3/" --space_width 3
--- TextRecognitionDataGenerator/gen_10_test.sh
+++ TextRecognitionDataGenerator/gen_10_test.sh
@@ -1,8 +1,8 @@
 #/bin/bash
-python run.py --language cn --length 13 --format 32 --loop_times 1 --output_dir "outs/train/black_1/"
-python run.py --language cn --length 13 --format 32 --loop_times 1 --output_dir "outs/test/black_1/"
-python run.py --language cn --length 13 --format 32 --loop_times 1 --output_dir "outs/train/black_1/" --text_color "#00B2EE"
-python run.py --language cn --length 13 --format 32 --loop_times 1 --output_dir "outs/test/black_1/" --text_color "#00B2EE"
+python run.py --language cn --length 14 --format 32 --loop_times 1 --output_dir "outs/train/black_1/"
+python run.py --language cn --length 14 --format 32 --loop_times 1 --output_dir "outs/test/black_1/"
+python run.py --language cn --length 14 --format 32 --loop_times 1 --output_dir "outs/train/black_1/" --text_color "#00B2EE"
+python run.py --language cn --length 14 --format 32 --loop_times 1 --output_dir "outs/test/black_1/" --text_color "#00B2EE"
 
 # ==========================================================================================================================================
 python run.py --language cn --length 10 --format 32 --loop_times 1 --output_dir "outs/train/black_3/" --space_width 3

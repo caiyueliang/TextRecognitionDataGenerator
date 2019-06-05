a5198ac23698343d945bb4cffaa00a8f23fb86f0
caiyueliang <393900414@qq.com>
2019-06-05 16:01:15 +0800

    update

--- TextRecognitionDataGenerator/computer_text_generator.py
+++ TextRecognitionDataGenerator/computer_text_generator.py
@@ -2,6 +2,7 @@ import random
 
 from PIL import Image, ImageColor, ImageFont, ImageDraw, ImageFilter
 
+
 def generate(text, font, text_color, font_size, orientation, space_width, fit):
     if orientation == 0:
         return _generate_horizontal_text(text, font, text_color, font_size, space_width, fit)
@@ -10,13 +11,47 @@ def generate(text, font, text_color, font_size, orientation, space_width, fit):
     else:
         raise ValueError("Unknown orientation " + str(orientation))
 
+
+# def _generate_horizontal_text(text, font, text_color, font_size, space_width, fit):
+#     image_font = ImageFont.truetype(font=font, size=font_size)
+#     words = text.split(' ')
+#     space_width = image_font.getsize(' ')[0] * space_width
+#
+#     words_width = [image_font.getsize(w)[0] for w in words]
+#     text_width = sum(words_width) + int(space_width) * (len(words) - 1)
+#     text_height = max([image_font.getsize(w)[1] for w in words])
+#
+#     txt_img = Image.new('RGBA', (text_width, text_height), (0, 0, 0, 0))
+#
+#     txt_draw = ImageDraw.Draw(txt_img)
+#
+#     colors = [ImageColor.getrgb(c) for c in text_color.split(',')]
+#     c1, c2 = colors[0], colors[-1]
+#
+#     fill = (
+#         random.randint(min(c1[0], c2[0]), max(c1[0], c2[0])),
+#         random.randint(min(c1[1], c2[1]), max(c1[1], c2[1])),
+#         random.randint(min(c1[2], c2[2]), max(c1[2], c2[2]))
+#     )
+#
+#     for i, w in enumerate(words):
+#         txt_draw.text((sum(words_width[0:i]) + i * int(space_width), 0), w, fill=fill, font=image_font)
+#
+#     if fit:
+#         return txt_img.crop(txt_img.getbbox())
+#     else:
+#         return txt_img
+
 def _generate_horizontal_text(text, font, text_color, font_size, space_width, fit):
     image_font = ImageFont.truetype(font=font, size=font_size)
-    words = text.split(' ')
-    space_width = image_font.getsize(' ')[0] * space_width
+    # TODO
+    # words = text.split(' ')
+    words = list(text)
+    # space_width = image_font.getsize(' ')[0] * space_width
 
     words_width = [image_font.getsize(w)[0] for w in words]
-    text_width =  sum(words_width) + int(space_width) * (len(words) - 1)
+    # text_width = sum(words_width) + int(space_width) * (len(words) - 1)
+    text_width = sum(words_width)
     text_height = max([image_font.getsize(w)[1] for w in words])
 
     txt_img = Image.new('RGBA', (text_width, text_height), (0, 0, 0, 0))
@@ -33,12 +68,14 @@ def _generate_horizontal_text(text, font, text_color, font_size, space_width, fi
     )
 
     for i, w in enumerate(words):
-        txt_draw.text((sum(words_width[0:i]) + i * int(space_width), 0), w, fill=fill, font=image_font)
+        # txt_draw.text((sum(words_width[0:i]) + i * int(space_width), 0), w, fill=fill, font=image_font)
+        txt_draw.text((sum(words_width[0:i]), 0), w, fill=fill, font=image_font)
 
     if fit:
         return txt_img.crop(txt_img.getbbox())
     else:
         return txt_img
+
 
 def _generate_vertical_text(text, font, text_color, font_size, space_width, fit):
     image_font = ImageFont.truetype(font=font, size=font_size)

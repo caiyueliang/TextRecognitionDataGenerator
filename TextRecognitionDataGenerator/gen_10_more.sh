420819bb8327cb54414314bd0e9bc77fcfb4f53b
caiyueliang <393900414@qq.com>
2019-06-06 15:39:12 +0800

    update

--- TextRecognitionDataGenerator/gen_10_more.sh
+++ TextRecognitionDataGenerator/gen_10_more.sh
@@ -1,31 +1,69 @@
 #/bin/bash
-#python run.py --language cn --length 14 --format 32 --loop_times 60 --output_dir "outs/train/black_1/"
-#python run.py --language cn --length 14 --format 32 --loop_times 30 --output_dir "outs/test/black_1/"
-#python run.py --language cn --length 14 --format 32 --loop_times 30 --output_dir "outs/train/black_1/" --text_color "#00B2EE"
-#python run.py --language cn --length 14 --format 32 --loop_times 15 --output_dir "outs/test/black_1/" --text_color "#00B2EE"
+#python run.py --language cn --length 8 --format 32 --loop_times 1 --output_dir "outs/train/black_1/" --width 200
+#python run.py --language cn --length 16 --format 32 --loop_times 1 --output_dir "outs/train/black_1/" --width 400
 
 # ==========================================================================================================================================
-#python run.py --language cn --length 10 --format 32 --loop_times 40 --output_dir "outs/train/black_3/" --space_width 3
-#python run.py --language cn --length 10 --format 32 --loop_times 20 --output_dir "outs/test/black_3/" --space_width 3
-#python run.py --language cn --length 10 --format 32 --loop_times 20 --output_dir "outs/train/black_3/" --text_color "#00B2EE" --space_width 3
-#python run.py --language cn --length 10 --format 32 --loop_times 10 --output_dir "outs/test/black_3/" --text_color "#00B2EE" --space_width 3
+#python run.py --language cn --length 6 --format 32 --loop_times 1 --output_dir "outs/train/black_3/" --space_width 3 --width 200
+#python run.py --language cn --length 9 --format 32 --loop_times 1 --output_dir "outs/train/black_3/" --space_width 3 --width 300
 
 # ==========================================================================================================================================
-#python run.py --language cn --length 8 --format 32 --loop_times 40 --output_dir "outs/train/black_5/" --space_width 5
-#python run.py --language cn --length 8 --format 32 --loop_times 20 --output_dir "outs/test/black_5/" --space_width 5
-#python run.py --language cn --length 8 --format 32 --loop_times 20 --output_dir "outs/train/black_5/" --text_color "#00B2EE" --space_width 5
-#python run.py --language cn --length 8 --format 32 --loop_times 10 --output_dir "outs/test/black_5/" --text_color "#00B2EE" --space_width 5
+#python run.py --language cn --length 7 --format 32 --loop_times 1 --output_dir "outs/train/black_5/" --space_width 5 --width 300
+#python run.py --language cn --length 13 --format 32 --loop_times 1 --output_dir "outs/train/black_5/" --space_width 5 --width 600
 
 # ==========================================================================================================================================
-python run.py --language cn --length 5 --format 32 --loop_times 5 --output_dir "outs/train/name/" --title "姓  名   " --width 300 --alignment 0
-python run.py --language cn --length 13 --format 32 --loop_times 10 --output_dir "outs/train/address/" --title "住  址   "
-python run.py --language cn --format 32 --count 5000 --output_dir "outs/train/sex/" --string_format "性  别   X      民  族 X" --width 300 --alignment 0
-python run.py --language cn --format 32 --count 5000 --output_dir "outs/train/birthday/" --string_format "出  生   XXXX  年 XX 月 XX 日" --width 300 --alignment 0 --dict "cn_num"
-python run.py --language cn --format 32 --count 5000 --output_dir "outs/train/id/" --string_format "公民身份号码    XXXXXXXXXXXXXXXXXX" --alignment 0 --dict "cn_numx"
+python run.py --language cn --length 8 --format 32 --loop_times 10 --output_dir "outs/train/200/" --width 200
+python run.py --language cn --length 16 --format 32 --loop_times 20 --output_dir "outs/train/400/" --width 400
+python run.py --language cn --length 8 --format 32 --loop_times 5 --output_dir "outs/test/200/" --width 200
+python run.py --language cn --length 16 --format 32 --loop_times 10 --output_dir "outs/test/400/" --width 400
+python run.py --language cn --length 8 --format 32 --loop_times 10 --output_dir "outs/train/200/" --width 200 --text_color "#00B2EE"
+python run.py --language cn --length 16 --format 32 --loop_times 20 --output_dir "outs/train/400/" --width 400 --text_color "#00B2EE"
+python run.py --language cn --length 8 --format 32 --loop_times 5 --output_dir "outs/test/200/" --width 200 --text_color "#00B2EE"
+python run.py --language cn --length 16 --format 32 --loop_times 10 --output_dir "outs/test/400/" --width 400 --text_color "#00B2EE"
 
 # ==========================================================================================================================================
-python run.py --language cn --length 5 --format 32 --loop_times 2 --output_dir "outs/test/name/" --title "姓  名   " --width 300 --alignment 0
-python run.py --language cn --length 13 --format 32 --loop_times 5 --output_dir "outs/test/address/" --title "住  址   "
-python run.py --language cn --format 32 --count 2000 --output_dir "outs/test/sex/" --string_format "性  别   X      民  族 X" --width 300 --alignment 0
-python run.py --language cn --format 32 --count 2000 --output_dir "outs/test/birthday/" --string_format "出  生   XXXX  年 XX 月 XX 日" --width 300 --alignment 0 --dict "cn_num"
-python run.py --language cn --format 32 --count 2000 --output_dir "outs/test/id/" --string_format "公民身份号码    XXXXXXXXXXXXXXXXXX" --alignment 0 --dict "cn_numx"
+python run.py --language cn --length 6 --format 32 --loop_times 10 --output_dir "outs/train/200/" --space_width 3 --width 200
+python run.py --language cn --length 9 --format 32 --loop_times 10 --output_dir "outs/train/300/" --space_width 3 --width 300
+python run.py --language cn --length 6 --format 32 --loop_times 5 --output_dir "outs/test/200/" --space_width 3 --width 200
+python run.py --language cn --length 9 --format 32 --loop_times 5 --output_dir "outs/test/300/" --space_width 3 --width 300
+python run.py --language cn --length 6 --format 32 --loop_times 10 --output_dir "outs/train/200/" --space_width 3 --width 200 --text_color "#00B2EE"
+python run.py --language cn --length 9 --format 32 --loop_times 10 --output_dir "outs/train/300/" --space_width 3 --width 300 --text_color "#00B2EE"
+python run.py --language cn --length 6 --format 32 --loop_times 5 --output_dir "outs/test/200/" --space_width 3 --width 200 --text_color "#00B2EE"
+python run.py --language cn --length 9 --format 32 --loop_times 5 --output_dir "outs/test/300/" --space_width 3 --width 300 --text_color "#00B2EE"
+
+# ==========================================================================================================================================
+python run.py --language cn --length 7 --format 32 --loop_times 10 --output_dir "outs/train/300/" --space_width 5 --width 300
+python run.py --language cn --length 13 --format 32 --loop_times 20 --output_dir "outs/train/600/" --space_width 5 --width 600
+python run.py --language cn --length 7 --format 32 --loop_times 5 --output_dir "outs/test/300/" --space_width 5 --width 300
+python run.py --language cn --length 13 --format 32 --loop_times 10 --output_dir "outs/test/600/" --space_width 5 --width 600
+python run.py --language cn --length 7 --format 32 --loop_times 10 --output_dir "outs/train/300/" --space_width 5 --width 300 --text_color "#00B2EE"
+python run.py --language cn --length 13 --format 32 --loop_times 20 --output_dir "outs/train/600/" --space_width 5 --width 600 --text_color "#00B2EE"
+python run.py --language cn --length 7 --format 32 --loop_times 5 --output_dir "outs/test/300/" --space_width 5 --width 300 --text_color "#00B2EE"
+python run.py --language cn --length 13 --format 32 --loop_times 10 --output_dir "outs/test/600/" --space_width 5 --width 600 --text_color "#00B2EE"
+
+# ==========================================================================================================================================
+python run.py --language cn --length 5 --format 32 --loop_times 4 --output_dir "outs/train/200/" --title "姓   名    " --width 200
+python run.py --language cn --length 13 --format 32 --loop_times 8 --output_dir "outs/train/400/" --title "住   址    " --width 400
+python run.py --language cn --format 32 --count 4000 --output_dir "outs/train/300/" --string_format "性   别    X          民   族  X" --width 300
+python run.py --language cn --format 32 --count 4000 --output_dir "outs/train/400/" --string_format "出   生      XXXX   年  XX  月  XX  日" --width 400 --dict "cn_num"
+python run.py --language cn --format 32 --count 4000 --output_dir "outs/train/600/" --string_format "公民身份号码      X X X X X X X X X X X X X X X X X X" --dict "cn_numx" --width 600
+
+# ==========================================================================================================================================
+python run.py --language cn --length 5 --format 32 --loop_times 2 --output_dir "outs/test/200/" --title "姓   名    " --width 200
+python run.py --language cn --length 13 --format 32 --loop_times 4 --output_dir "outs/test/400/" --title "住   址    " --width 400
+python run.py --language cn --format 32 --count 2000 --output_dir "outs/test/300/" --string_format "性   别    X          民   族  X" --width 300
+python run.py --language cn --format 32 --count 2000 --output_dir "outs/test/400/" --string_format "出   生      XXXX   年  XX  月  XX  日" --width 400 --dict "cn_num"
+python run.py --language cn --format 32 --count 2000 --output_dir "outs/test/600/" --string_format "公民身份号码      X X X X X X X X X X X X X X X X X X" --dict "cn_numx" --width 600
+
+# ==========================================================================================================================================
+python run.py --language cn --length 5 --format 32 --loop_times 4 --output_dir "outs/train/200/" --title "姓   名    " --width 200 --text_color "#00B2EE"
+python run.py --language cn --length 13 --format 32 --loop_times 8 --output_dir "outs/train/400/" --title "住   址    " --width 400 --text_color "#00B2EE"
+python run.py --language cn --format 32 --count 4000 --output_dir "outs/train/300/" --string_format "性   别    X          民   族  X" --width 300 --text_color "#00B2EE"
+python run.py --language cn --format 32 --count 4000 --output_dir "outs/train/400/" --string_format "出   生      XXXX   年  XX  月  XX  日" --width 400 --dict "cn_num" --text_color "#00B2EE"
+python run.py --language cn --format 32 --count 4000 --output_dir "outs/train/600/" --string_format "公民身份号码      X X X X X X X X X X X X X X X X X X" --dict "cn_numx" --width 600 --text_color "#00B2EE"
+
+# ==========================================================================================================================================
+python run.py --language cn --length 5 --format 32 --loop_times 2 --output_dir "outs/test/200/" --title "姓   名    " --width 200 --text_color "#00B2EE"
+python run.py --language cn --length 13 --format 32 --loop_times 4 --output_dir "outs/test/400/" --title "住   址    " --width 400 --text_color "#00B2EE"
+python run.py --language cn --format 32 --count 2000 --output_dir "outs/test/300/" --string_format "性   别    X          民   族  X" --width 300 --text_color "#00B2EE"
+python run.py --language cn --format 32 --count 2000 --output_dir "outs/test/400/" --string_format "出   生      XXXX   年  XX  月  XX  日" --width 400 --dict "cn_num" --text_color "#00B2EE"
+python run.py --language cn --format 32 --count 2000 --output_dir "outs/test/600/" --string_format "公民身份号码      X X X X X X X X X X X X X X X X X X" --dict "cn_numx" --width 600 --text_color "#00B2EE"
